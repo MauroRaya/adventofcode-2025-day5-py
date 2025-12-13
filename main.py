4278772b3268abf1411354a8176013b6395e16da
MauroRaya <maurorayabr@gmail.com>
2025-12-13 14:50:36 -0300

    refactor: compare binary search and standard iteration

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-def is_input_in_range(start: int, end: int, target: int) -> int:
-    l = start
-    r = end
+import time
+from typing import Callable, Any
+
+
+def time_it(func: Callable, *args, **kwargs) -> Any:
+    start: float = time.perf_counter()
+    result: Any = func(*args, **kwargs)
+    end: float = time.perf_counter()
+
+    print(f"Tempo decorrido: {end - start:.6f}s")
+    return result
+
+
+def binary_search(start: int, end: int, target: int) -> int:
+    l: int = start
+    r: int = end
 
     while l <= r:
-        m = (l + r) // 2
+        m: int = (l + r) // 2
 
         if target > m:
             l = m + 1
@@ -15,27 +28,42 @@ def is_input_in_range(start: int, end: int, target: int) -> int:
     return -1
 
 
-def main():
-    count = 0
-
-    ranges = open('files/ranges.txt').read().splitlines()
-    lines  = open('files/inputs.txt')
-
-    for line in lines:
-        input = line.strip()
+def standard_iteration(start: int, end: int, target: int) -> int:
+    for i in range(start, end + 1):
+        if target == i:
+            return i
         
-        for range in ranges:
-            nums: list[str] = range.split('-')
+    return -1
 
-            if is_input_in_range(
-                int(nums[0]), 
-                int(nums[1]), 
-                int(input)
-            ) != -1:
-                count += 1
-                break
 
-    print(count)
+def count_available_fresh_ingridient_ids(
+    search_func: Callable[[int, int, int], int]
+) -> int:
+    count: int = 0
+
+    with open('files/ranges.txt') as f1:
+        ranges: list[str] = f1.read().splitlines()
+
+    with open('files/inputs.txt') as f2:
+        for line in f2:
+            input: int = int(line.strip())
+
+            for range in ranges:
+                nums: list[str] = range.split('-')
+
+                start: int = int(nums[0])
+                end:   int = int(nums[1])
+
+                if search_func(start, end, input) != -1:
+                    count += 1
+                    break
+
+    return count
+
+
+def main():
+    time_it(count_available_fresh_ingridient_ids, binary_search)
+    time_it(count_available_fresh_ingridient_ids, standard_iteration)
 
 
 if __name__ == '__main__':
