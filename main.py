fd81cd62ce264c5bec00b246719f23c83b70147f
MauroRaya <maurorayabr@gmail.com>
2025-12-14 11:52:54 -0300

    refactor: simplify lookup code for ingredient_id

--- main.py
+++ main.py
@@ -1,45 +1,6 @@
-import time
-from typing import Callable, Any
-
-
-def time_it(func: Callable, *args, **kwargs) -> Any:
-    start: float = time.perf_counter()
-    result: Any = func(*args, **kwargs)
-    end: float = time.perf_counter()
-
-    print(f"Tempo decorrido: {end - start:.6f}s")
-    return result
-
-
-def binary_search(start: int, end: int, target: int) -> int:
-    l: int = start
-    r: int = end
-
-    while l <= r:
-        m: int = (l + r) // 2
-
-        if target > m:
-            l = m + 1
-        elif target < m:
-            r = m - 1
-        else:
-            return m
-
-    return -1
-
-
-def standard_iteration(start: int, end: int, target: int) -> int:
-    for i in range(start, end + 1):
-        if target == i:
-            return i
-        
-    return -1
-
-
 def count_available_fresh_ingredients_ids(
     fresh_ingredients_ranges_ids_path: str,
-    ingredients_ids_path: str,
-    search_func: Callable[[int, int, int], int]
+    ingredients_ids_path: str
 ) -> int:
     count: int = 0
 
@@ -56,7 +17,7 @@ def count_available_fresh_ingredients_ids(
                 start: int = int(nums[0])
                 end:   int = int(nums[1])
 
-                if search_func(start, end, ingredient_id) != -1:
+                if start <= ingredient_id <= end:
                     count += 1
                     break
 
@@ -64,21 +25,10 @@ def count_available_fresh_ingredients_ids(
 
 
 def main():
-    print("Contando ingredientes frescos usando busca binária...")
-    count = time_it(
-        count_available_fresh_ingredients_ids, 
+    print("Contando ingredientes frescos...")
+    count = count_available_fresh_ingredients_ids(
         'files/ingredients/available/fresh/range-ids.txt',
-        'files/ingredients/available/unknown/ids.txt', 
-        binary_search
-    )
-    print(f"{count} ingredientes frescos encontrados\n")
-
-    print("Contando ingredientes frescos usando uma busca ordinária...")
-    count = time_it(
-        count_available_fresh_ingredients_ids, 
-        'files/ingredients/available/fresh/range-ids.txt',
-        'files/ingredients/available/unknown/ids.txt', 
-        standard_iteration
+        'files/ingredients/available/unknown/ids.txt'
     )
     print(f"{count} ingredientes frescos encontrados\n")
 
